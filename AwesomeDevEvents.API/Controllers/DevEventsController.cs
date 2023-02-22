7a0abab77d4e568371652d30c09b0b9bdaae866a
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-22 19:27:42 -0300

    Adicionar arquivos de projeto.

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+    }
+}
